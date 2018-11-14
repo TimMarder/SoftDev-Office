c9fd335dd77cdaf1444975097e4b2e442a87a70c
TimMarder <tmarder@stuy.edu>
2018-11-13 22:06:20 -0500

    finished K24

---
+++ 24_rest/rest.py
@@ -0,0 +1,25 @@
+# Tim Marder
+# SoftDev1 pd06
+# K#23 -- A RESTful Journey Skyward
+# 2018-11-13
+
+from flask import Flask, render_template
+import urllib, json
+
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/")       #assign fxn to route
+def api():
+    url = "https://api.nasa.gov/planetary/apod?api_key=ueoj9lycxVpsdpUDymbW43EjGxbl2h99DWdatUu3"
+    pic = "https://i.pinimg.com/originals/ab/50/51/ab505151f51ca72dedb8ac1ac689b2cb.jpg"
+    req = urllib.request.urlopen( url )
+    data = req.read()
+    dictionary = json.loads( data )
+    #print(dictionary)
+    return render_template("rest.html",
+                            img = dictionary[ 'url' ],
+                            explanation = dictionary[ 'explanation' ])
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
